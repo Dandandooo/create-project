4ed4adceb894d89f4aeb9dfe08a69ac5969074c4
Daniel Philipov <batkodanny@gmail.com>
2024-02-24 23:31:34 -0600

    build script

---
+++ src/build.rs
@@ -0,0 +1,32 @@
+use std::process::Command;
+use crate::langs::supported_languages;
+use std::collections::HashSet;
+
+fn main() {
+    let ignores = compile_ignores();
+
+    for ignore in ignores {
+        install_gitignore(ignore);
+    }
+}
+
+fn compile_ignores() -> Vec<HashSet<String>> {
+    let mut ignores = Vec::new();
+
+    let langs = supported_languages();
+    for (lang, properties) in langs.into_iter() {
+        for ignore in properties.get("ignore").unwrap() {
+            ignores.push(ignore.clone());
+        }
+    }
+
+    ignores
+}
+
+fn install_gitignore(set: HashSet<String>) {
+    let gitignore = set.join(",");
+
+    let sorted_filenames = set.into_iter().collect::<Vec<String>>().sort().join("_");
+    
+    Command::new("curl").args([format!("https://www.toptal.com/developers/gitignore/api/{gitignore}", ">", format!("./langs/gitignores/{sorted_filenames}.txt")]).spawn().expect("Failed to install gitignore");
+}
